95dc120b1ded43b136c805f69b605064d1b20e59
Rocky <yangrocky2013@gmail.com>
2021-01-19 22:02:20 -0600

    practice linked Lists

--- quick_sort.js
+++ quick_sort.js
@@ -1,22 +1,35 @@
 
 
-const input = [10, -2, 1, 0, -14, 4, 9, 6, 14, -40, 43, 41, -21]
 
+const input = [5,1,-1,4,10,-14,7,11]
 // WITH FILTER METHOD
+// const quickSort = (arr) => {
+//     if(arr.length <= 1) return arr
+
+//     let pivot = arr.shift();
+//     // let left = arr.filter(el => el < pivot);
+//     // let right = arr.filter(el => el >= pivot);
+
+//     let left = []
+//     let right = []
+
+//     arr.forEach(el => {
+//         if(el < pivot) left.push(el)
+//         else if (el >= pivot) right.push(el)
+//     })
+
+//     let leftSorted = quickSort(left)
+//     let rightSorted = quickSort(right)
+
+//     return [...leftSorted, pivot, ...rightSorted]
+// }
+
 const quickSort = (arr) => {
     if(arr.length <= 1) return arr
 
     let pivot = arr.shift();
-    // let left = arr.filter(el => el < pivot);
-    // let right = arr.filter(el => el >= pivot);
-
-    let left = []
-    let right = []
-
-    arr.forEach(el => {
-        if(el < pivot) left.push(el)
-        else if (el >= pivot) right.push(el)
-    })
+    let left = arr.filter(el => el < pivot)
+    let right = arr.filter(el => el >= pivot)
 
     let leftSorted = quickSort(left)
     let rightSorted = quickSort(right)
