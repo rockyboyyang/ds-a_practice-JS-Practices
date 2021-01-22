547aaa0d3a41f3da01f60529edd6238b0a2d7f38
Rocky <yangrocky2013@gmail.com>
2021-01-22 11:21:55 -0600

    practice binary search

--- dsa_projects/6_binary_search_project/lib/binary_search.js
+++ dsa_projects/6_binary_search_project/lib/binary_search.js
@@ -1,11 +1,31 @@
 function binarySearch(array, target) {
+    if(array.length === 0) return false
 
+    let midIdx = Math.floor(array.length / 2) 
+    let leftHalf = array.slice(0, midIdx)
+    let rightHalf = array.slice(midIdx + 1)
+
+    if(target < array[midIdx]) return binarySearch(leftHalf, target)
+    else if(target > array[midIdx]) return binarySearch(rightHalf, target)
+    else return true
 }
 
 function binarySearchIndex(array, target) {
+    if (!array.length) return -1
 
+    let midIdx = Math.floor(array.length / 2)
+    let leftHalf = array.slice(0, midIdx)
+    let rightHalf = array.slice(midIdx + 1)
+
+    if (target < array[midIdx]) return binarySearchIndex(leftHalf, target)
+    else if (target > array[midIdx]) return binarySearchIndex(rightHalf, target)
+    else return midIdx
 }
 
+let arr1 = [-4, ,0, 1, 3, 8 , 12, 20, 25, 32, 64, 190, 299]
+
+console.log(binarySearchIndex(arr1, 13))
+console.log(binarySearchIndex(arr1, 299))
 
 module.exports = {
     binarySearch,
