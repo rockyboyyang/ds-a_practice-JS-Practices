ce3f96406311b4ddbe42237c4a5fb87a757aa290
Rocky <yangrocky2013@gmail.com>
2021-01-25 17:41:47 -0600

    practice queues

--- dsa_projects/9_stacks_and_queues/queue_project/lib/queue.js
+++ dsa_projects/9_stacks_and_queues/queue_project/lib/queue.js
@@ -21,11 +21,51 @@
 // -----------
 
 class Node {
-
+    constructor(val) {
+        this.value = val
+        this.next = null
+    }
 }
 
 class Queue {
+    constructor() {
+        this.front = null
+        this.back = null
+        this.length = 0
+    }
 
+    enqueue(val) {
+        let newNode = new Node(val)
+        if(!this.front) {
+            this.front = newNode
+            this.back = newNode
+        } else {
+            let oldBack = this.back
+            oldBack.next = newNode
+            this.back = newNode
+        }
+
+        this.length ++
+        return this.length
+    }
+
+    dequeue() {
+        let removedNode = this.front
+        if(!this.length) return null
+        else if(this.length === 1) {
+            this.front = null
+            this.back = null
+        } else {
+            this.front = this.front.next
+        }
+
+        this.length --
+        return removedNode.value
+    }
+
+    size() {
+        return this.length
+    }
 }
 
 exports.Node = Node;
