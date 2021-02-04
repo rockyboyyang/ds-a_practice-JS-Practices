d83b065b65f1ef9d472e5b40ce3a0cf2bde30050
Rocky <yangrocky2013@gmail.com>
2021-02-04 15:18:07 -0600

    worked on tree heights

--- dsa_projects/10_trees/bst_project/lib/leet_code_110.js
+++ dsa_projects/10_trees/bst_project/lib/leet_code_110.js
@@ -1,7 +1,19 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+/*
+Approach:
 
-function isBalanced(root) {
+*/
+// root level, left is 1, right is 2
+function isBalanced(root, currentLevel = 0) {
+    if(!root) return 0
+    let leftHeight = isBalanced(root.left, currentLevel + 1) + 1 
+    let rightHeight = isBalanced(root.right, currentLevel + 1) + 1
 
+    if(currentLevel === 0) return Math.abs(leftHeight - rightHeight) <= 1
+    else {
+        if(leftHeight > rightHeight) return leftHeight
+        else return rightHeight
+    }
 }
