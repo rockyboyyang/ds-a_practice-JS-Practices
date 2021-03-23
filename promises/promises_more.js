f1e484669fc624a42ccad7c04c47722d4d99ffae
Rocky <yangrocky2013@gmail.com>
2021-03-23 14:04:34 -0500

    promises

--- promises/promises_more.js
+++ promises/promises_more.js
@@ -1,10 +1,10 @@
-// How to create a promise?
-// const promise = new Promise()
+// // How to create a promise?
+// // const promise = new Promise()
 
-// How to full fill or reject the promise?
-// const promise = new Promise(() => {
+// // How to full fill or reject the promise?
+// // const promise = new Promise(() => {
 
-// })
+// // })
 
 // let isTacos = false
 
@@ -18,6 +18,10 @@
 //     console.log('Start cooking pasta!')
 // }
 
+// const onFinally = () => {
+//     console.log('promise over')
+// }
+
 // const promise = new Promise((resolve, reject) => {
 //     if(isTacos) {
 //         setTimeout(() => {
@@ -26,11 +30,50 @@
 //     }
 //     else {
 //         setTimeout(() => {
-//             reject('Sorry, no tacos!')
+//             reject('Sorry, no tacos!')   
 //         }, 3000)
 //     }
+//     // resolve('Bringing Tacos')
+//     // reject('Sorry, no tacos!')
+
 // })
 
 // promise
 //     .then(onFullfillment)
 //     .catch(onRejection)
+//     .finally(onFinally)
+
+// // onFullfillment()
+
+// // let firstFunction = function() {
+// //     return new Promise((resolve, reject) => {
+// //         resolve('Your cat is ')
+// //     })
+// // }
+
+// // firstFunction().then(res => console.log(res))
+
+// function isAnagram(str1, str2) {
+//     let str1Len = str1.length
+//     let str2Len = str2.length
+//     if(str1Len !== str2Len) return false
+//     let charFreq = {}
+
+//     for(let char of str1) {
+//         if(charFreq[char]) charFreq[char] ++
+//         else charFreq[char] = 1
+//     }
+
+//     for(let char of str2) {
+//         if (charFreq[char]) charFreq[char]--
+//         else return false
+//     }
+
+//     return true
+
+// }
+
+// console.log(isAnagram('abcde', 'decab')) // true
+// console.log(isAnagram('abcde', 'deceb')) // false
+// console.log(isAnagram('rhinos', 'sorhin')) // true
+// console.log(isAnagram('abcde', 'abc')) // false
